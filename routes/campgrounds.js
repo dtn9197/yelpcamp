0d204fa4f33888fbda11537d26c243400ef914ef
Duy <dtn9197@gmail.com>
2020-08-18 20:20:41 -0400

    commit

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
-const campground = require("../models/campground");
+var middleware = require("../middleware");
 
 //INDEX ROUTE - show all campgrounds
 router.get("/", function(req, res) {
@@ -19,9 +19,9 @@ router.get("/", function(req, res) {
 });
 
 //NEW - show form to create new campground 
-router.get("/new", isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function(req, res){
     res.render("campgrounds/new") 
- });
+});
 
 //SHOW - shows more info about campground selected - to be declared after NEW to not overwrite
 router.get("/:id", function(req, res){
@@ -41,7 +41,7 @@ router.get("/:id", function(req, res){
 
 
 //CREATE - add new campgrounds to database
-router.post("/", isLoggedIn, function (req, res){
+router.post("/", middleware.isLoggedIn, function (req, res){
     // get data from form and add to campgrounds array
     var name= req.body.name;
     var image = req.body.image;
@@ -67,7 +67,7 @@ router.post("/", isLoggedIn, function (req, res){
 
 
 // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
+router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground) {
         res.render("campgrounds/edit", {campground: foundCampground});
 
@@ -75,7 +75,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 });
 
 // UPDATE ROUTE
-router.put("/:id", checkCampgroundOwnership, function(req, res) {
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res) {
     //find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
@@ -88,7 +88,7 @@ router.put("/:id", checkCampgroundOwnership, function(req, res) {
 });
 
 //DESTROY Campground Route
-router.delete("/:id", checkCampgroundOwnership, function(req, res) {
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             res.redirect("/campgrounds");
@@ -100,41 +100,5 @@ router.delete("/:id", checkCampgroundOwnership, function(req, res) {
 
 
 
-/**this is used to make sure user is logged in before
- * they can progress, otherwise send them to login page
- */
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
-
-function checkCampgroundOwnership(req, res, next) {
-    if(req.isAuthenticated()) {
-        Campground.findById(req.params.id, function(err, foundCampground) {
-            if(err) {
-                //back means redirect back to the page user was before this call
-                res.redirect("/back");
-            } else {
-                //does user id matches the author of campground
-                /** the id in the campground is a mongoose object, not a string
-                 * therefore use the mongoose method to compare
-                 */
-                if(foundCampground.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    res.redirect("back");
-                    console.log("You don't have permission");
-                }   
-    
-            }
-        })
-
-    } else {
-        console.log("you need to log in to do that");
-        res.redirect("back");
-    }
-}
 
 module.exports = router;
--- routes/comments.js
+++ routes/comments.js
@@ -6,7 +6,7 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
-const { response } = require("express");
+var middleware = require("../middleware");
 
 
 
@@ -14,7 +14,7 @@ const { response } = require("express");
  * to check whether user is logged in before they can post ocmments
  */
 
-router.get("/new", isLoggedIn, function(req, res) {
+router.get("/new", middleware.isLoggedIn, function(req, res) {
     console.log("comments new route hit");
     //find campground by id
     Campground.findById(req.params.id, function(err, campground) {
@@ -29,7 +29,7 @@ router.get("/new", isLoggedIn, function(req, res) {
 });
 
 //comment create
-router.post("/",isLoggedIn, function(req, res) {
+router.post("/",middleware.isLoggedIn, function(req, res) {
     /**lookup campground by id
      * create a comment
      *    associate the userID with the comments then save it
@@ -64,7 +64,7 @@ router.post("/",isLoggedIn, function(req, res) {
 });
 
 //COMMENT EDIT ROUTE
-router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             res.redirect("back");
@@ -76,7 +76,7 @@ router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 });
 
 //COMMENT UPDATE
-router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             res.redirect("back");
@@ -87,7 +87,7 @@ router.put("/:comment_id", checkCommentOwnership, function(req, res) {
 });
 
 //COMMENT DESTROY ROUTE
-router.delete("/:comment_id",checkCommentOwnership, function(req, res) {
+router.delete("/:comment_id",middleware.checkCommentOwnership, function(req, res) {
     //findByIdAndRemove
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
@@ -99,42 +99,5 @@ router.delete("/:comment_id",checkCommentOwnership, function(req, res) {
 });
 
 
-
-/**this is used to make sure user is logged in before
- * they can progress, otherwise send them to login page
- */
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
-function checkCommentOwnership(req, res, next) {
-    if(req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function(err, foundComment) {
-            if(err) {
-                //back means redirect back to the page user was before this call
-                res.redirect("/back");
-            } else {
-                //does user id matches the comment they want to edit?
-                /** the id in the comment is a mongoose object, not a string
-                 * therefore use the mongoose method to compare
-                 */
-                if(foundComment.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    console.log("You don't have permission");
-                    res.redirect("back");
-                    
-                }   
-    
-            }
-        })
-
-    } else {
-        console.log("you need to log in to do that");
-        res.redirect("back");
-    }
-}
 
 module.exports = router;
